e9e2bab252e27000d1b86a77b02804256be059a1
Jackson Wilson <jacksonwilson99@gmail.com>
2021-12-12 23:04:57 -0600

    Add broken code

--- server/iot-car-tracker-server.py
+++ server/iot-car-tracker-server.py
@@ -1,26 +1,53 @@
 from aiohttp import web
 import socketio
 import obd
+import time, serial
 
-sio = socketio.AsyncServer(async_mode='aiohttp', logger=True,
-                           engineio_logger=True, cors_allowed_origins=['http://localhost:4200'])
+sio = socketio.AsyncServer(
+    async_mode="aiohttp",
+    logger=True,
+    engineio_logger=True,
+    cors_allowed_origins=["http://localhost:4200"],
+)
 
 app = web.Application()
 sio.attach(app)
 
-connection = obd.OBD("/dev/rfcomm99")#, timeout=1)
+
+# connection = obd.OBD(
+#     "/dev/rfcomm99", protocol="6", baudrate="9600", fast=False, timeout=30
+# )
+# while len(connection.supported_commands) < 100:
+#     connection = obd.OBD(
+#         "/dev/rfcomm99", protocol="6", baudrate="9600", fast=False, timeout=30
+#     )
+
 
 def readCarStatus(self):
-    gasLevel = connection.query(obd.commands.FUEL_LEVEL)
-    print(gasLevel)
-    gearSelected = connection.query(obd.commands[1][164])
-    print(gearSelected)
-    odometer = connection.query(obd.commands[1][166])
-    print(odometer)
-    speed = connection.query(obd.commands.SPEED)
-    print(speed)
-    vin = connection.query(obd.commands[9][2])
-    print(vin)
+    # gasLevel = connection.query(obd.commands.FUEL_LEVEL)
+    # print(gasLevel)
+    # gearSelected = connection.query(obd.commands[1][164])
+    # print(gearSelected)
+    # odometer = connection.query(obd.commands[1][166])
+    # print(odometer)
+    # speed = connection.query(obd.commands.SPEED)
+    # print(speed)
+    # vin = connection.query(obd.commands[9][2])
+    # print(vin)
+
+    port = "/dev/ttyACM0"
+    ser = serial.Serial(port)
+    ser.baudrate = 9600
+    
+    line = ser.readline()
+    line2 = line.decode("latin-1")
+    while not line2.startswith("$GPGGA"):
+        line = ser.readline()
+        line2 = line.decode("latin-1")
+    lat, dir1, long, dir2 = line2.split(",")[2:6]
+    print(lat)
+    print(long)
+    print(line)
     return {
         "gasLevel": "40",
         "gearSelected": "Park",
@@ -28,7 +55,7 @@ def readCarStatus(self):
         "odometer": "125000",
         "speed": "0",
         "time": "6:32",
-        "vin": "31231241412"
+        "vin": "31231241412",
     }
 
 
@@ -54,20 +81,21 @@ trips_json = [
         "updatesReceived": "ee",
         "gasUsed": "ff",
         "averageSpeed": "gg",
-    }
+    },
 ]
 
 
-@sio.on('getCarStatus')
+@sio.on("getCarStatus")
 async def handle_message(self):
     print("Sending car status")
-    await sio.emit('carStatus', readCarStatus())
+    await sio.emit("carStatus", readCarStatus(self))
 
 
-@sio.on('getTrips')
+@sio.on("getTrips")
 async def handle_message(self):
     print("Sending trips")
-    await sio.emit('trips', trips_json)
+    await sio.emit("trips", trips_json)
 
-if __name__ == '__main__':
-    web.run_app(app, host="localhost", port=4444)
+
+if __name__ == "__main__":
+    web.run_app(app, host="172.20.10.6", port=4444)
